7fa043ee749e43947690968a64a9988a3d37f920
Julien PONCELET <jponcele@e2r7p3.42.fr>
2014-04-05 18:37:56 +0200

    save via alias

--- htdocs/includes/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-	<hr />
-	<h5>Copyright &copy; 42.fr - jponcele - vrey - 2014</h5>
-</footer>
--- htdocs/index.php
+++
@@ -1,7 +0,0 @@
-<?PHP
-include("sqli_connect.php");
-	session_start();
-require_once("./includes/header.php");
-require_once("./includes/footer.php");
-
-?>
