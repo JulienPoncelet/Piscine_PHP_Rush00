981af2898cead5d43127d992af2627efe1c5d13c
Julien PONCELET <jponcele@e2r7p3.42.fr>
2014-04-06 17:01:54 +0200

    save via alias

--- htdocs/admin/ft_delete_member.php
+++
@@ -1,7 +0,0 @@
-<?php
-include ("../mysqli/sqli_connect.php");
-$db = sql_connect();
-$name = $_POST['name'];
-$query = "DELETE FROM `user` WHERE `login` = '$name'";
-mysqli_query($db, $query);
-?>
--- htdocs/items/ft_delete_item.php
+++
@@ -1,7 +0,0 @@
-<?php
-include ("../mysqli/sqli_connect.php");
-$db = sql_connect();
-$name = $_POST['name'];
-$query = "DELETE FROM `product` WHERE `ID` = '$name'";
-mysqli_query($db, $query);
-?>
