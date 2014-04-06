1356611bf9da1eac18c7d308a0bb4bb0cd5800b6
Julien PONCELET <jponcele@e2r7p3.42.fr>
2014-04-06 19:07:59 +0200

    save via alias

--- htdocs/includes/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-	<h5>Copyright &copy; 42.fr - jponcele - vrey - 2014</h5>
-</footer>
--- htdocs/logout/logout.php
+++
@@ -1,7 +0,0 @@
-<?PHP
-session_start();
-$_SESSION['login'] = NULL;
-$_SESSION['admin'] = "0";
-unset($_SESSION);
-header ("Location: /index.php");
-?>
