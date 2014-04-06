3c4cec381522a06a493b21bbd1468f289964642b
Julien PONCELET <jponcele@e2r7p3.42.fr>
2014-04-06 14:40:34 +0200

    save via alias

---
+++ htdocs/account/modify_passwd.php
@@ -0,0 +1,23 @@
+<?PHP
+
+session_start();
+if (isset($_SESSION['login']))
+	require_once("../includes/header_login.php");
+else
+	require_once("../includes/header_logout.php");
+
+require_once("../includes/footer.php");
+?>
+
+<div class="login">
+	<h1>Modification du Mot de Passe</h1>
+	<form action="modify.php" method="POST" class="form_login">
+		<div id="login_passwd">
+			<label for="">Old Pwd : </label><input id="input" type="password" name="old_passwd"/><br />
+			<label for="">New Pwd : </label><input id="input" type="password" name="new_passwd"/><br />
+		</div>
+		<div>
+			<input class="bouton" type="submit" name="submit" value="OK"/>
+		</div>
+	</form>
+</div>
---
+++ htdocs/includes/header_admin.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<link href="../style/style.css" type="text/css" rel="stylesheet"/>
+	</head>
+	<header>
+		<div>
+			<h1>42 E-Commerce</h1>
+			<div class="user">
+				User : <?PHP print($_SESSION["login"])?>
+			</div>
+			<div class="user">
+				<a href="../logout/logout.php">Logout</a>
+			</div>
+		</div>
+		<ul class="menu">
+			<li><a href="/index.php">Home</a></li>
+			<li><a href="/index.php">Shop</a></li>
+			<li><a href="../account/index.php">My Account</a></li>
+			<li><a href="/admin/index.php">Admin</a></li>
+		</ul>
+	</header>
+</html>
